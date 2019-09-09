df7806124e9d61455a31a8d4cb619ead397a088e
labartist <labartist@zoho.com>
2019-09-08 20:15:19 -0700

    initial additions to components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule
